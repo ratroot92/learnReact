34fa11168325317ef600a70b8764995fe2e45567
ratroot92 <maliksblr92@gmail.com>
2021-04-14 23:21:51 +0500

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+
+
+function Header(){
+    return (<nav className="navbar navbar-expand-lg navbar-light bg-dark">
+    <a className="navbar-brand text-white" href="#">React Developers</a>
+    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+  
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav mr-auto">
+        <li className="nav-item active">
+          <a className="nav-link text-white" href="#">Home <span className="sr-only">(current)</span></a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link text-white" href="#">Link</a>
+        </li>
+        <li className="nav-item dropdown">
+          <a className="nav-link text-white dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Dropdown
+          </a>
+          <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+            <a className="dropdown-item" href="#">Action</a>
+            <a className="dropdown-item" href="#">Another action</a>
+            <div className="dropdown-divider"></div>
+            <a className="dropdown-item" href="#">Something else here</a>
+          </div>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link text-white disabled" href="#">Disabled</a>
+        </li>
+      </ul>
+     
+    </div>
+  </nav>)
+}
+
+
+export default Header
---
+++ src/components/ShopPage.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import products from './../data/data'
+
+
+
+
+
+
+function ShopPage (){
+
+
+
+
+    const arr=[1,2,3,4,5,6];
+
+
+    
+
+    return (<div className="card">
+        <div className="card-header"></div>
+        <div className="card-img"></div>
+        <div className="card-body"></div>
+        <div className="card-footer"></div>
+    </div>)
+}
