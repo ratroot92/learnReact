0d4a821738e554b184fa5eb373e2787cf3b246a5
ratroot92 <maliksblr92@gmail.com>
2021-04-19 23:28:24 +0500

    ...

---
+++ src/components/ShopCompoenents/CategoryStrip.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+ function CategoryStrip() {
+    return (
+        <div className=" mt-2 border border-success p-2 col-md-12 d-flex flex-row justify-content-around align-items-center">
+            <button 
+            type="button"
+            className="btn btn-sm btn-primary text-white"
+            >
+            Fruits</button>
+            <button 
+            type="button"
+            className="btn btn-sm btn-primary text-white"
+            >
+            Vegetables</button>
+            <button 
+            type="button"
+            className="btn btn-sm btn-primary text-white"
+            >
+            Soups</button>
+        </div>
+    )
+}
+export default CategoryStrip
--- src/components/ShopPage.jsx
+++ src/components/ShopPage.jsx
@@ -4,6 +4,30 @@ import products from './../data/data'
 
 
 
+//parent compoenent  pass cart ka object child component ko 
+
+//parent compoenent ka pass aik state ha => cart
+//parent compoenent na apne Child Compoenent jo ka => <Cart></Cart> 
+// aik paramerter pass kiya ha 
+// us para meter k pass current cart ka data ha 
+// const Cart=(props)
+// ander Object Destrcutre 
+// const {abc,cart}=props
+const Cart=({cart,abc})=>{
+    // const Cart=(props)=>{
+    //     const {cart}=props
+    console.log('====================================');
+    console.log("cart",cart);
+    console.log("abc",abc);
+    console.log("cart.totalItems",cart.totalItems);
+    console.log("cart.totalItems",cart.totalPrice);
+    console.log('====================================');
+    return (
+    <div className="">
+        <p>Total Items : {cart.totalItems }</p>
+        <p>Total Price : {cart.totalPrice }</p>
+    </div>)
+}
 
 
 
@@ -11,17 +35,42 @@ function ShopPage (){
 
 
 const [state,setState]=React.useState(products)
+const [cart,setCart]=React.useState({
+    totalPrice:0,
+    totalItems:0
+})
 
+
+
+
+
+const addToCart=(product)=>{
+console.log('====================================');
+console.log("item",product.price);
+console.log('====================================');
+//totalItems:cart.totalItems+1
+//,
+//totalPrice:cart.totalPrice+item.price
+// wrap {}
+setCart({totalItems:cart.totalItems+1,totalPrice:cart.totalPrice+product.price})
+}
+
+
+
+
+const resetCart= ()=>{
+    setCart({totalItems:0,totalPrice:0})
+}
    
 
 
 
     return (
         <>
+        <Cart cart={cart} abc={"abc"}></Cart>
+        <button type="button" className="btn btn-sm btn-warning" onClick={resetCart}>Reset Cart</button>
         {
             state.map((product,index)=>{
-                console.log(product)
-                console.log(index)
                 return (
                     <div className="col-md-4 p-3"  key={product.name}>
                     <div className="card border border-dark p-2">
@@ -33,7 +82,7 @@ const [state,setState]=React.useState(products)
                             Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis iure impedit eius iusto itaque quod tenetur corrupti! Maxime consequatur, unde eos repudiandae rem omnis corporis dicta, optio cupiditate earum suscipit.
                         </div>
                         <div className="card-footer text-center">
-                            <button className="btn btn-sm btn-success">Add to cart 
+                            <button onClick={()=>{addToCart(product)}} className="btn btn-sm btn-success">Add to cart 
                             $-{product.price}</button>
                         </div>
                     </div>
